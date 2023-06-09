cee6af3e58cad04d0e13840c5bfaf13506faa0e5
Muhammad Saddam <saddam.fastnu@gmail.com>
2023-06-09 19:59:13 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ app.use(cookieParser());
 const PORT = process.env.PORT || 3000;
 
 app.get('/',(req, res) => {
-  res.cookie(`custom_cookie`,`custom cookie value`);
-  res.send(ip.address() + '\nCookie have been saved successfully');
+  res.send('Host IP: '+ ip.address() +'<br>Custom cookie value: '+ req.cookies.custom_cookie);
+});
+
+app.get('/set_custom_cookie',(req, res) => {
+  res.cookie(`custom_cookie`,`EDUCATIVE`);
+  res.send('Host IP: '+ ip.address() + '<br>Custom cookie has been saved successfully');
 });
 
 app.listen(PORT, () => console.log('Server listening at port 3000'));
